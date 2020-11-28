84f40bc6f8fcc884c84ff83bcafbcd711b366216
SakharovDN <dimka.puhlik@gmail.com>
2020-11-28 16:27:56 +0600

    feature(HomeController): Добавлен метод FileIsValid, который проверяет файл на соответствие ограничениям (размер файла, расширение). feature(WordDocument): Улучшен алгоритм поиска слов в документе

--- OnlineReviewer/Controllers/HomeController.cs
+++ OnlineReviewer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using OnlineReviewer.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -11,10 +12,6 @@ namespace OnlineReviewer.Controllers
 {
     public class HomeController : Controller
     {
-        private Word.Application app;
-
-        private Word.Document doc;
-
         public ActionResult Index()
         {
             ViewBag.FileExists = false;
@@ -22,40 +19,69 @@ namespace OnlineReviewer.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
+        public ActionResult Review(HttpPostedFileBase file)
         {
-            app = new Word.Application();
-            if (file != null && file.ContentLength > 0)
-                try
-                {
-                    string pathFile = Server.MapPath("~/App_Data/Uploads/") + file.FileName;
-                    file.SaveAs(pathFile);
-                    doc = WorkDocument.OpenDoc(pathFile, app);
-                    WorkDocument.CorrectDoc(doc);
-                    WorkDocument.SaveDoc(app);
-                    ViewBag.Message = "Файл успешно загружен";
-                    ViewBag.FileName = file.FileName;
-                    ViewBag.FileExists = true;
-                }
-                catch (Exception ex)
-                {
-                    ViewBag.Message = "ERROR: " + ex.Message.ToString();
-                    ViewBag.FileExists = false;
-                }
-            else
+            Word.Application app = new Word.Application();
+            Word.Document doc;
+            try
             {
-                ViewBag.Message = "Файл не выбран";
+                FileIsValid(file);
+                //Загружаем файл на сервер
+                string pathFile = Server.MapPath("~/App_Data/Uploads/") + file.FileName;
+                file.SaveAs(pathFile);
+                
+                doc = WordDocument.Open(pathFile, app);
+                WordDocument.Review(doc);
+                WordDocument.Quit(app);
+                //Объекты, которые передаются в Home/Index.cshtml
+                //Сообщение об успешной загрузке и рецензировании документа
+                ViewBag.Message = "Файл успешно загружен";
+                //Имя файла документа, при нажатии на ссылку "Скачать файл" передается этот объект в метод DownloadFile
+                ViewBag.FileName = file.FileName;
+                //Рецензированный файл на сервере существует и рецензирован без ошибки
+                ViewBag.FileExists = true;
+            }
+            catch(Exception ex)
+            {
+                //Объекты, которые передаются в Home/Index.cshtml
+                //Сообщение об ошибке
+                ViewBag.Message = ex.Message.ToString();
+                //Рецензированный файл на сервере не существует или произошла ошибка при реценизровании
                 ViewBag.FileExists = false;
             }
-            return View();
+            return View("Index");
         }
-
+        /// <summary>
+        /// Метод отправляющий файл из сервера к клиенту
+        /// </summary>
+        /// <param name="fileName">Имя скачиваемого файла</param>
+        /// <returns></returns>
         public FileResult DownloadFile(string fileName)
         {
             string pathFile = Server.MapPath("~/App_Data/Uploads/") + fileName;
             string contentType = "application/docx";
             string fileDownloadName = "reviewed" + fileName;
             return File(pathFile, contentType, fileDownloadName);
+        }
+        /// <summary>
+        /// Метод, который проверяет файл на соответствие ограничениям (размер файла, расширение)
+        /// </summary>
+        /// <param name="file"></param>
+        private void FileIsValid(HttpPostedFileBase file)
+        {
+            if (file == null || file.ContentLength == 0)
+            {
+                throw new ArgumentException("Файл не выбран");
+            }
+            string extension = Path.GetExtension(file.FileName);
+            if (extension != ".docx" && extension != ".doc")
+            {
+                throw new ArgumentException("Расширение файла должно быть .docx или .doc");
+            }
+            if (file.ContentLength > 1e7)
+            {
+                throw new ArgumentException("Файл слишком большой");
+            }
         }
 
         public ActionResult About()
---
+++ OnlineReviewer/Models/WordDocument.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace OnlineReviewer.Models
+{
+    public static class WordDocument
+    {
+
+        /// <summary>
+        /// Открывает и возвращает документ Word
+        /// </summary>
+        /// <param name="pathFile">Путь до файла</param>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static Word.Document Open(string pathFile, Word.Application app)
+        {
+            app.Visible = false;
+            Object fileName = pathFile;
+            Word.Document doc = app.Documents.Open(ref fileName);
+            return doc;
+        }
+
+        /// <summary>
+        /// Метод, который ищет ошибки в документе
+        /// </summary>
+        /// <param name="doc">Документ, в котором ищутся ошибки</param>
+        public static void Review(Word.Document doc)
+        {
+            var dictionary = InitializeDictionary();
+
+            foreach(var wrongWord in dictionary)
+            {
+                //Весь контент документа присваивается области range
+                Word.Range range = doc.Content;
+                //Удаляем форматирование
+                range.Find.ClearFormatting();
+                //Поиск вперед по документу
+                range.Find.Forward = true;
+                //Текст, который ищется
+                range.Find.Text = wrongWord.Key;
+                //Выполнить поиск слова
+                range.Find.Execute();
+                //.Found возвращает true, если слово найдено
+                while (range.Find.Found)
+                {
+                    //Добавляется примечание
+                    range.Comments.Add(range, wrongWord.Value);
+                    //Поиск слова
+                    range.Find.Execute();
+                }
+            }
+        }
+        /// <summary>
+        /// Метод, который закрывает документ, сохраняя все изменения
+        /// </summary>
+        /// <param name="wordapp"></param>
+        public static void Quit(Word.Application wordapp)
+        {
+            Object saveChanges = Word.WdSaveOptions.wdSaveChanges;
+            Object originalFormat = Word.WdOriginalFormat.wdOriginalDocumentFormat;
+            wordapp.Quit(ref saveChanges, ref originalFormat);
+        }
+
+        private static Dictionary<string,string> InitializeDictionary()
+        {
+            string dictionaryContent = File.ReadAllText(HostingEnvironment.MapPath("~/App_Data/dictionary.json"));
+            Dictionary<string, string> dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(dictionaryContent);
+            return dictionary;
+        }
+    }
+}
